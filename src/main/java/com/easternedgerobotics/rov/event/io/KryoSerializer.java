a402445e4c6286a2135b52d31ccd9e8043f9f965
ccp412 <ccp412@mun.ca>
2015-11-07 10:57:01 -0330

    Fix thread safety for KryoSerializer

--- src/main/java/com/easternedgerobotics/rov/event/io/KryoSerializer.java
+++ src/main/java/com/easternedgerobotics/rov/event/io/KryoSerializer.java
@@ -9,20 +9,19 @@ import com.esotericsoftware.kryo.io.Output;
  * The KryoSerializer class serializes and deserializes Java objects
  * using <a href="https://github.com/EsotericSoftware/kryo">Kryo</a>.
  * <p>
- * This class is not thread safe because Kryo is not thread safe.
+ * This class is thread safe.
  */
 public class KryoSerializer implements Serializer {
-    /**
-     * The Kryo instance.
-     */
-    private final Kryo kryo;
-
-    public KryoSerializer() {
-        kryo = new Kryo();
-    }
+    private final ThreadLocal<Kryo> threadLocalKryo  = new ThreadLocal<Kryo>() {
+        @Override
+        protected Kryo initialValue() {
+            return new Kryo();
+        }
+    };
 
     @Override
     public final byte[] serialize(final Object value) {
+        final Kryo kryo = threadLocalKryo.get();
         final Output output = new FastOutput(16, 1024);
         kryo.writeClassAndObject(output, value);
         return output.toBytes();
@@ -30,6 +29,7 @@ public class KryoSerializer implements Serializer {
 
     @Override
     public final Object deserialize(final byte[] bytes) {
+        final Kryo kryo = threadLocalKryo.get();
         return kryo.readClassAndObject(new FastInput(bytes));
     }
 }
