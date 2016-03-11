08abd7de6788955866966597b1b1471c47ce189e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-03-11 17:22:57 -0330

    Add CPU observable for voltage, freq, temperature

---
+++ src/main/java/com/easternedgerobotics/rov/io/CpuInformation.java
@@ -0,0 +1,45 @@
+package com.easternedgerobotics.rov.io;
+
+import com.easternedgerobotics.rov.value.CpuValue;
+
+import com.pi4j.system.SystemInfo;
+import rx.Observable;
+import rx.exceptions.Exceptions;
+import rx.schedulers.Schedulers;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public final class CpuInformation {
+    private final Observable<Long> interval;
+
+    /**
+     * Constructs a CpuInformation instance that polls CPU properties at the specified interval.
+     *
+     * @param interval the interval at which to poll the CPU properties.
+     * @param timeUnit the {@code TimeUnit} the interval is specified in.
+     */
+    public CpuInformation(final int interval, final TimeUnit timeUnit) {
+        this.interval = Observable.interval(interval, timeUnit);
+    }
+
+    /**
+     * Returns an observable stream of CPU values.
+     *
+     * @return a stream of CPU values.
+     */
+    public final Observable<CpuValue> observe() {
+        return interval.observeOn(Schedulers.io())
+            .map(this::pollCpu)
+            .observeOn(Schedulers.immediate());
+    }
+
+    private CpuValue pollCpu(final long tick) {
+        try {
+            return CpuValue.create(
+                SystemInfo.getClockFrequencyArm(), SystemInfo.getCpuTemperature(), SystemInfo.getCpuVoltage());
+        } catch (final InterruptedException | IOException e) {
+            throw Exceptions.propagate(e);
+        }
+    }
+}
