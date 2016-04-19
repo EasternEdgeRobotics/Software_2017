cc73c0ae8b46b3cc6ad31b86f3e9cd25eb2b1535
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-04-18 23:05:38 -0230

    Rework SliderView to have its own controller

--- src/main/java/com/easternedgerobotics/rov/fx/ThrusterPowerSlidersView.java
+++ src/main/java/com/easternedgerobotics/rov/fx/ThrusterPowerSlidersView.java
@@ -11,28 +11,9 @@ public class ThrusterPowerSlidersView implements View {
 
     final HBox row = new HBox(SPACING);
 
-    final SliderView globalSliderView = new SliderView("Global", 0);
-
-    final SliderView heaveSliderView = new SliderView("Heave", SliderView.MAX_VALUE);
-
-    final SliderView swaySliderView = new SliderView("Sway", SliderView.MAX_VALUE);
-
-    final SliderView surgeSliderView = new SliderView("Surge", SliderView.MAX_VALUE);
-
-    final SliderView yawSliderView = new SliderView("Yaw", SliderView.MAX_VALUE);
-
-    final SliderView rollSliderView = new SliderView("Roll", SliderView.MAX_VALUE);
-
     @Inject
     public ThrusterPowerSlidersView() {
         row.setPadding(new Insets(SPACING));
-        row.getChildren().addAll(
-            globalSliderView.getParent(),
-            heaveSliderView.getParent(),
-            swaySliderView.getParent(),
-            surgeSliderView.getParent(),
-            yawSliderView.getParent(),
-            rollSliderView.getParent());
     }
 
     @Override
--- src/main/java/com/easternedgerobotics/rov/fx/ThrusterPowerSlidersViewController.java
+++ src/main/java/com/easternedgerobotics/rov/fx/ThrusterPowerSlidersViewController.java
@@ -22,44 +22,110 @@ public class ThrusterPowerSlidersViewController implements ViewController {
     private final ThrusterPowerSlidersView view;
 
     /**
+     * The slider for global power.
+     */
+    private final SliderView globalSliderView;
+
+    /**
+     * The slider for power in the heave direction.
+     */
+    private final SliderView heaveSliderView;
+
+    /**
+     * The slider for power in the sway direction.
+     */
+    private final SliderView swaySliderView;
+
+    /**
+     * The slider for power in the surge direction.
+     */
+    private final SliderView surgeSliderView;
+
+    /**
+     * The slider for power in the pitch direction.
+     */
+    private final SliderView pitchSliderView;
+
+    /**
+     * The slider for power in the yaw direction.
+     */
+    private final SliderView yawSliderView;
+
+    /**
+     * The slider for power in the roll direction.
+     */
+    private final SliderView rollSliderView;
+
+    /**
      * The subscriptions held by this view controller.
      */
     private final CompositeSubscription subscriptions;
 
     /**
-     * Constructs a @{code ThrusterPowerSlidersViewController} with the specified event publisher and view.
+     * Constructs a {@code ThrusterPowerSlidersViewController} with the specified event publisher and view.
      *
      * @param eventPublisher the event publisher instance to use
-     * @param view the @{code ThrusterPowerSlidersView} this controller works for
+     * @param view the {@code ThrusterPowerSlidersView} this controller works for
+     * @param globalSliderView the global power slider view
+     * @param heaveSliderView the heave power slider view
+     * @param swaySliderView the sway power slider view
+     * @param surgeSliderView the surge power slider view
+     * @param pitchSliderView the pitch power slider view
+     * @param yawSliderView the yaw power slider view
+     * @param rollSliderView the roll power slider view
      */
     @Inject
     public ThrusterPowerSlidersViewController(
         final EventPublisher eventPublisher,
-        final ThrusterPowerSlidersView view
+        final ThrusterPowerSlidersView view,
+        final SliderView globalSliderView,
+        final SliderView heaveSliderView,
+        final SliderView swaySliderView,
+        final SliderView surgeSliderView,
+        final SliderView pitchSliderView,
+        final SliderView yawSliderView,
+        final SliderView rollSliderView
     ) {
         this.eventPublisher = eventPublisher;
         this.view = view;
+        this.globalSliderView = globalSliderView;
+        this.heaveSliderView = heaveSliderView;
+        this.swaySliderView = swaySliderView;
+        this.surgeSliderView = surgeSliderView;
+        this.pitchSliderView = pitchSliderView;
+        this.yawSliderView = yawSliderView;
+        this.rollSliderView = rollSliderView;
         this.subscriptions = new CompositeSubscription();
     }
 
     @Override
     public final void onCreate() {
-        updateLabel(view.globalSliderView);
-        updateLabel(view.heaveSliderView);
-        updateLabel(view.swaySliderView);
-        updateLabel(view.surgeSliderView);
-        updateLabel(view.yawSliderView);
-        updateLabel(view.rollSliderView);
+        globalSliderView.displayNameLabel.setText("Global");
+        heaveSliderView.displayNameLabel.setText("Heave");
+        swaySliderView.displayNameLabel.setText("Sway");
+        surgeSliderView.displayNameLabel.setText("Surge");
+        yawSliderView.displayNameLabel.setText("Yaw");
+        rollSliderView.displayNameLabel.setText("Roll");
 
-        final Observable<Float> global = values(view.globalSliderView);
-        final Observable<Float> heave = values(view.heaveSliderView);
-        final Observable<Float> sway = values(view.swaySliderView);
-        final Observable<Float> surge = values(view.surgeSliderView);
-        // We don't actually show pitch, but it's nice to have pitch in the model,
-        // so we want to emit a value for it.
-        final Observable<Float> pitch = values(new SliderView("Pitch", 0));
-        final Observable<Float> yaw = values(view.yawSliderView);
-        final Observable<Float> roll = values(view.rollSliderView);
+        // Safety first!
+        globalSliderView.slider.setValue(0);
+
+        view.row.getChildren().addAll(
+            globalSliderView.getParent(),
+            heaveSliderView.getParent(),
+            swaySliderView.getParent(),
+            surgeSliderView.getParent(),
+            yawSliderView.getParent(),
+            rollSliderView.getParent()
+        );
+
+        final Observable<Float> global = values(globalSliderView);
+        final Observable<Float> heave = values(heaveSliderView);
+        final Observable<Float> sway = values(swaySliderView);
+        final Observable<Float> surge = values(surgeSliderView);
+        final Observable<Float> pitch = values(pitchSliderView);
+        final Observable<Float> yaw = values(yawSliderView);
+        final Observable<Float> roll = values(rollSliderView);
 
         subscriptions.add(
             Observable.combineLatest(global, heave, sway, surge, pitch, yaw, roll, MotionPowerValue::create)
@@ -70,16 +136,6 @@ public class ThrusterPowerSlidersViewController implements ViewController {
     @Override
     public final void onDestroy() {
         subscriptions.unsubscribe();
-    }
-
-    private String labelText(final Number value) {
-        return String.format(SliderView.LABEL_FORMAT, value.floatValue());
-    }
-
-    private void updateLabel(final SliderView sliderView) {
-        subscriptions.add(JavaFxObservable.fromObservableValue(sliderView.slider.valueProperty())
-            .map(this::labelText)
-            .subscribe(sliderView.valueLabel::setText));
     }
 
     private Observable<Float> values(final SliderView sliderView) {
