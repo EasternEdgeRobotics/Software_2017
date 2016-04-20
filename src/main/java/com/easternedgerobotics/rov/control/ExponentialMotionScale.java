7ce6b8a95514d984f0a08d491e275c34d41da6a1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-04-20 15:32:57 -0230

    Add exponential motion value scale

---
+++ src/main/java/com/easternedgerobotics/rov/control/ExponentialMotionScale.java
@@ -0,0 +1,36 @@
+package com.easternedgerobotics.rov.control;
+
+import com.easternedgerobotics.rov.value.MotionValue;
+
+import java.util.function.Function;
+
+public class ExponentialMotionScale implements Function<MotionValue, MotionValue> {
+    /**
+     * Applies exponential scaling to the given motion value.
+     *
+     * @param motionValue the input motion value
+     * @return the scaled motion value output
+     */
+    @Override
+    public final MotionValue apply(final MotionValue motionValue) {
+        return MotionValue.create(
+            scale(motionValue.getHeave()),
+            scale(motionValue.getSway()),
+            scale(motionValue.getSurge()),
+            scale(motionValue.getPitch()),
+            scale(motionValue.getYaw()),
+            scale(motionValue.getRoll())
+        );
+    }
+
+    /**
+     * Apply exponential scaling to the input.
+     * <p>
+     * See <a href="https://github.com/EasternEdgeRobotics/2016/issues/107">#107</a> for background.
+     * @param x the function input
+     * @return the scaled value
+     */
+    private static float scale(final float x) {
+        return (float) ((Math.exp(x) - 1) / (Math.E - 1));
+    }
+}
