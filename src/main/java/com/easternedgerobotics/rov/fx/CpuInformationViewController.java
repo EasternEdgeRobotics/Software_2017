e906df24ba901d144cc34206cdb4cd531f1368b7
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-05-04 12:09:38 -0230

    Add CpuInformationView and controller

---
+++ src/main/java/com/easternedgerobotics/rov/fx/CpuInformationViewController.java
@@ -0,0 +1,40 @@
+package com.easternedgerobotics.rov.fx;
+
+import com.easternedgerobotics.rov.event.Event;
+import com.easternedgerobotics.rov.value.CpuValue;
+
+import rx.Observable;
+import rx.subscriptions.CompositeSubscription;
+
+import javax.inject.Inject;
+
+public class CpuInformationViewController implements ViewController {
+    private final Observable<CpuValue> rasprime;
+
+    private final CpuInformationView view;
+
+    private final CompositeSubscription subscriptions;
+
+    @Inject
+    public CpuInformationViewController(@Event final Observable<CpuValue> rasprime, final CpuInformationView view) {
+        this.rasprime = rasprime;
+        this.view = view;
+        this.subscriptions = new CompositeSubscription();
+    }
+
+    @Override
+    public final void onCreate() {
+        subscriptions.add(rasprime.subscribe(this::updateLabels));
+    }
+
+    @Override
+    public final void onDestroy() {
+        subscriptions.unsubscribe();
+    }
+
+    private void updateLabels(final CpuValue value) {
+        view.frequencyLabel.setText(Long.toString(value.getFrequency()));
+        view.temperatureLabel.setText(Float.toString(value.getTemperature()));
+        view.voltageLabel.setText(Float.toString(value.getVoltage()));
+    }
+}
