fe77d8a9cdc36b20100625927306940bc26e7b3d
ccp412 <ccp412@mun.ca>
2016-05-17 12:00:25 -0230

    Add class for every speed type

--- src/main/kotlin/com/easternedgerobotics/rov/value/SpeedValue.kt
+++ src/main/kotlin/com/easternedgerobotics/rov/value/SpeedValue.kt
@@ -1,9 +1,5 @@
 package com.easternedgerobotics.rov.value
 
-data class SpeedValue(val name: String = "", val speed: Float = 0f) {
-    constructor(name: String) : this(name, 0f)
-
-    fun setSpeed(speed: Float) : SpeedValue {
-        return this.copy(this.name, speed);
-    }
+interface SpeedValue {
+    val speed : Float
 }
