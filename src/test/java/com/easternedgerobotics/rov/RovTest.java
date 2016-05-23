e2e2f8f543ec479d3f705d45c1de6e9f87d89b3d
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-05-23 00:25:06 -0230

    Add test to verify motion is being used correctly

--- src/test/java/com/easternedgerobotics/rov/RovTest.java
+++ src/test/java/com/easternedgerobotics/rov/RovTest.java
@@ -4,9 +4,13 @@ import com.easternedgerobotics.rov.io.ADC;
 import com.easternedgerobotics.rov.io.PWM;
 import com.easternedgerobotics.rov.test.TestEventPublisher;
 import com.easternedgerobotics.rov.value.HeartbeatValue;
+import com.easternedgerobotics.rov.value.MotionPowerValue;
+import com.easternedgerobotics.rov.value.MotionValue;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.hamcrest.MockitoHamcrest;
 import rx.schedulers.TestScheduler;
 
 import java.util.AbstractList;
@@ -114,5 +118,43 @@ public class RovTest {
         Mockito.verify(maestro.get(Rov.PORT_FORE_CHANNEL)).write(0);
         Mockito.verify(maestro.get(Rov.PORT_VERT_CHANNEL)).write(0);
         Mockito.verify(maestro.get(Rov.PORT_AFT_CHANNEL)).write(0);
+    }
+
+    @Test
+    public final void doesWriteMotionValueAndPowerInputToThrusters() {
+        final TestScheduler scheduler = new TestScheduler();
+        final TestEventPublisher eventPublisher = new TestEventPublisher(scheduler);
+        final MockMaestro maestro = new MockMaestro();
+        final Rov rov = new Rov(eventPublisher, maestro);
+
+        rov.init(scheduler, scheduler);
+        eventPublisher.testObserver(HeartbeatValue.class).onNext(new HeartbeatValue(true));
+        eventPublisher.testObserver(MotionPowerValue.class).onNext(new MotionPowerValue(1, 1, 1, 1, 1, 1, 1));
+        eventPublisher.testObserver(MotionValue.class).onNext(new MotionValue(0, 0, 1, 0, 0, 0));
+        scheduler.advanceTimeBy(Rov.SLEEP_DURATION, TimeUnit.MILLISECONDS);
+        scheduler.advanceTimeBy(Rov.SLEEP_DURATION, TimeUnit.MILLISECONDS);
+
+        Mockito.verify(maestro.get(Rov.STARBOARD_FORE_CHANNEL)).writeZero();
+        Mockito.verify(maestro.get(Rov.STARBOARD_VERT_CHANNEL)).writeZero();
+        Mockito.verify(maestro.get(Rov.STARBOARD_AFT_CHANNEL)).writeZero();
+        Mockito.verify(maestro.get(Rov.PORT_FORE_CHANNEL)).writeZero();
+        Mockito.verify(maestro.get(Rov.PORT_VERT_CHANNEL)).writeZero();
+        Mockito.verify(maestro.get(Rov.PORT_AFT_CHANNEL)).writeZero();
+
+        Mockito.verify(maestro.get(Rov.STARBOARD_FORE_CHANNEL)).write(0);
+        Mockito.verify(maestro.get(Rov.STARBOARD_FORE_CHANNEL)).write(MockitoHamcrest.floatThat(CoreMatchers.not(0f)));
+
+        Mockito.verify(maestro.get(Rov.STARBOARD_VERT_CHANNEL), Mockito.times(2)).write(0);
+
+        Mockito.verify(maestro.get(Rov.STARBOARD_AFT_CHANNEL)).write(0);
+        Mockito.verify(maestro.get(Rov.STARBOARD_AFT_CHANNEL)).write(MockitoHamcrest.floatThat(CoreMatchers.not(0f)));
+
+        Mockito.verify(maestro.get(Rov.PORT_FORE_CHANNEL)).write(0);
+        Mockito.verify(maestro.get(Rov.PORT_FORE_CHANNEL)).write(MockitoHamcrest.floatThat(CoreMatchers.not(0f)));
+
+        Mockito.verify(maestro.get(Rov.PORT_VERT_CHANNEL), Mockito.times(2)).write(0);
+
+        Mockito.verify(maestro.get(Rov.PORT_AFT_CHANNEL)).write(0);
+        Mockito.verify(maestro.get(Rov.PORT_AFT_CHANNEL)).write(MockitoHamcrest.floatThat(CoreMatchers.not(0f)));
     }
 }
