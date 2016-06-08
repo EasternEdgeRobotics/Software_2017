7bee432f638da44e0d0356b8ef67fc22e67d6e73
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-06-07 21:38:29 -0230

    Rewrite Sensor view to inline most values

--- src/main/java/com/easternedgerobotics/rov/fx/SensorViewController.java
+++ src/main/java/com/easternedgerobotics/rov/fx/SensorViewController.java
@@ -1,7 +1,18 @@
 package com.easternedgerobotics.rov.fx;
 
+import com.easternedgerobotics.rov.event.Event;
+import com.easternedgerobotics.rov.value.ExternalPressureValueA;
+import com.easternedgerobotics.rov.value.ExternalPressureValueB;
+import com.easternedgerobotics.rov.value.ExternalTemperatureValue;
+import com.easternedgerobotics.rov.value.InternalPressureValue;
+import com.easternedgerobotics.rov.value.InternalTemperatureValue;
+
+import rx.Observable;
+import rx.subscriptions.CompositeSubscription;
+
 import javax.inject.Inject;
 
+@SuppressWarnings("unused")
 public class SensorViewController implements ViewController {
     /**
      * The sensor view.
@@ -9,39 +20,77 @@ public class SensorViewController implements ViewController {
     private final SensorView view;
 
     /**
-     * The internal temperature sensor sub-view.
-     */
-    private final InternalTemperatureSensorView internalTemperatureSensorView;
-
-    /**
-     * The internal pressure sensor sub-view.
-     */
-    private final InternalPressureSensorView internalPressureSensorView;
-
-    /**
      * The CPU information sub-view.
      */
     private final CpuInformationView cpuInformationView;
 
+    private final Observable<InternalPressureValue> internalPressure;
+
+    private final Observable<ExternalPressureValueA> externalPressureA;
+
+    private final Observable<ExternalPressureValueB> externalPressureB;
+
+    private final Observable<InternalTemperatureValue> internalTemperature;
+
+    private final Observable<ExternalTemperatureValue> externalTemperature;
+
+    private final CompositeSubscription subscriptions;
+
     @Inject
     public SensorViewController(
         final SensorView view,
-        final InternalTemperatureSensorView internalTemperatureSensorView,
-        final InternalPressureSensorView internalPressureSensorView,
-        final CpuInformationView cpuInformationView
+        final CpuInformationView cpuInformationView,
+        @Event final Observable<InternalPressureValue> internalPressure,
+        @Event final Observable<ExternalPressureValueA> externalPressureA,
+        @Event final Observable<ExternalPressureValueB> externalPressureB,
+        @Event final Observable<InternalTemperatureValue> internalTemperature,
+        @Event final Observable<ExternalTemperatureValue> externalTemperature
     ) {
         this.view = view;
-        this.internalTemperatureSensorView = internalTemperatureSensorView;
-        this.internalPressureSensorView = internalPressureSensorView;
         this.cpuInformationView = cpuInformationView;
+        this.subscriptions = new CompositeSubscription();
+
+        this.internalPressure = internalPressure;
+        this.externalPressureA = externalPressureA;
+        this.externalPressureB = externalPressureB;
+        this.internalTemperature = internalTemperature;
+        this.externalTemperature = externalTemperature;
     }
 
     @Override
     public final void onCreate() {
-        view.row.getChildren().addAll(
-            internalTemperatureSensorView.getParent(),
-            internalPressureSensorView.getParent(),
-            cpuInformationView.getParent()
-        );
+        view.row.getChildren().add(cpuInformationView.getParent());
+        subscriptions.add(internalPressure.observeOn(jfxScheduler).subscribe(this::updatePressureLabel));
+        subscriptions.add(externalPressureA.observeOn(jfxScheduler).subscribe(this::updatePressureLabel));
+        subscriptions.add(externalPressureB.observeOn(jfxScheduler).subscribe(this::updatePressureLabel));
+        subscriptions.add(internalTemperature.observeOn(jfxScheduler).subscribe(this::updateTemperatureLabel));
+        subscriptions.add(externalTemperature.observeOn(jfxScheduler).subscribe(this::updateTemperatureLabel));
+    }
+
+    @Override
+    public final void onDestroy() {
+        subscriptions.unsubscribe();
+    }
+
+    private void updatePressureLabel(final InternalPressureValue value) {
+        view.internalPressureLabel.setText(String.format(SensorView.PRESSURE_LABEL_FORMAT, value.getPressure()));
+    }
+
+    private void updatePressureLabel(final ExternalPressureValueA value) {
+        view.externalPressureLabelA.setText(String.format(SensorView.PRESSURE_LABEL_FORMAT, value.getValue()));
+    }
+
+    private void updatePressureLabel(final ExternalPressureValueB value) {
+        view.externalPressureLabelB.setText(String.format(SensorView.PRESSURE_LABEL_FORMAT, value.getValue()));
+    }
+
+    private void updateTemperatureLabel(final InternalTemperatureValue internalTemperatureValue) {
+        view.internalTemperatureLabel.setText(
+            String.format(SensorView.TEMPERATURE_LABEL_FORMAT, internalTemperatureValue.getTemperature()));
+    }
+
+    private void updateTemperatureLabel(final ExternalTemperatureValue internalTemperatureValue) {
+        view.externalTemperatureLabel.setText(
+            String.format(SensorView.TEMPERATURE_LABEL_FORMAT, internalTemperatureValue.getValue()));
     }
 }
