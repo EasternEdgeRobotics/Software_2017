96c5f9ebce341c77a4abba0e8bcb00c7f32b4d23
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-06-21 15:57:23 -0500

    Don't truncate floating point numbers

--- src/main/java/com/easternedgerobotics/rov/math/MovingAverage.java
+++ src/main/java/com/easternedgerobotics/rov/math/MovingAverage.java
@@ -6,7 +6,7 @@ public final class MovingAverage {
     public static Observable<Float> from(final Observable<Float> source, final int n) {
         final RingBuffer buffer = new RingBuffer(n);
         return source.map(value -> {
-            buffer.add(value.longValue());
+            buffer.add(value);
             return buffer.apply(s -> (float) s.summaryStatistics().getAverage());
         });
     }
