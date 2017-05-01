a2c4d393ba3aa44ed246c6b55ab6ad6ed2dea846
Cal Pratt <cpratt@mun.ca>
2017-05-01 12:46:39 -0230

    Analog to powerlevel to use float not int

--- src/main/java/com/easternedgerobotics/rov/control/AnalogToPowerLevel.java
+++ src/main/java/com/easternedgerobotics/rov/control/AnalogToPowerLevel.java
@@ -7,22 +7,22 @@ public final class AnalogToPowerLevel {
     /**
      * Used to scale the value to the [0, 110] range.
      */
-    private static final int SCALAR = 110;
+    private static final float SCALAR = 1.10f;
 
     /**
      * Used to shift the value to the [-5, 105] range.
      */
-    private static final int OFFSET = 5;
+    private static final float OFFSET = 0.05f;
 
     /**
      * Lower bound for the result.
      */
-    private static final int MINIMUM = 0;
+    private static final float MINIMUM = 0;
 
     /**
      * Upper bound for the result.
      */
-    private static final int MAXIMUM = 100;
+    private static final float MAXIMUM = 1.00f;
 
     /**
      * Transform an analog pin value into an appropriate power slider value
@@ -31,7 +31,7 @@ public final class AnalogToPowerLevel {
      * @param analogValue the input.
      * @return power slider value.
      */
-    public static int convert(final float analogValue) {
+    public static float convert(final float analogValue) {
         return clamp(((1 - analogValue) * SCALAR) - OFFSET);
     }
 
@@ -41,13 +41,13 @@ public final class AnalogToPowerLevel {
      * @param value the power slider value.
      * @return clamped value.
      */
-    private static int clamp(final float value) {
+    private static float clamp(final float value) {
         if (value < MINIMUM) {
             return MINIMUM;
         }
         if (value > MAXIMUM) {
             return MAXIMUM;
         }
-        return Math.round(value);
+        return value;
     }
 }
