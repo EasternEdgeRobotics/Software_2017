0bd7a3db8c95c8528b934177ae7cf24d450a8eb6
Cal Pratt <cpratt@mun.ca>
2017-05-25 00:55:20 -0230

    Add javadoc to calibration results

--- src/main/java/com/easternedgerobotics/rov/video/CameraCalibrationResult.java
+++ src/main/java/com/easternedgerobotics/rov/video/CameraCalibrationResult.java
@@ -3,25 +3,48 @@ package com.easternedgerobotics.rov.video;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Size;
 
+import java.util.Collections;
 import java.util.List;
 
 public final class CameraCalibrationResult {
+    /**
+     * The files used to create the calibration result.
+     */
     private final List<String> validFileNames;
 
+    /**
+     * The size of the calibrated image.
+     */
     private final Size imageSize;
 
     /**
-     * calibrateCamera returns the root mean square (RMS) re-projection error,
+     * The root mean square (RMS) re-projection error of the calibration
      * usually it should be between 0.1 and 1.0 pixels in a good calibration.
      *
      * See http://stackoverflow.com/a/29897948/3280538
      */
     private final double calibrateCameraRmsError;
 
+    /**
+     * This intrinsic camera properties.
+     */
     private final Mat cameraMatrix;
 
+    /**
+     * This distortion coefficients associated with the camera.
+     * Turns real world coordinates into image space coordinates.
+     */
     private final Mat distortionCoeffs;
 
+    /**
+     * The results from running a camera calibration.
+     *
+     * @param validFileNames The files used to create the calibration result.
+     * @param imageSize The size of the calibrated image.
+     * @param calibrateCameraRmsError The root mean square (RMS) re-projection error of the calibration
+     * @param cameraMatrix This intrinsic camera matrix.
+     * @param distortionCoeffs The distortion coefficients associated with the camera.
+     */
     public CameraCalibrationResult(
         final List<String> validFileNames,
         final Size imageSize,
@@ -36,22 +59,37 @@ public final class CameraCalibrationResult {
         this.distortionCoeffs = distortionCoeffs;
     }
 
+    /**
+     * @return The files used to create the calibration result.
+     */
     public List<String> getValidFileNames() {
-        return validFileNames;
+        return Collections.unmodifiableList(validFileNames);
     }
 
+    /**
+     * @return The size of the calibrated image.
+     */
     public Size getImageSize() {
         return imageSize;
     }
 
+    /**
+     * @return The root mean square (RMS) re-projection error of the calibration
+     */
     public double getCalibrateCameraRmsError() {
         return calibrateCameraRmsError;
     }
 
+    /**
+     * @return This intrinsic camera matrix.
+     */
     public Mat getCameraMatrix() {
         return cameraMatrix;
     }
 
+    /**
+     * @return The distortion coefficients associated with the camera.
+     */
     public Mat getDistortionCoeffs() {
         return distortionCoeffs;
     }
