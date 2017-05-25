a6a675db09ddb49154daf6382f2938a1b3b2dc36
Cal Pratt <cpratt@mun.ca>
2017-05-25 01:00:28 -0230

    Add initial work to distance calculator tool

---
+++ src/main/java/com/easternedgerobotics/rov/math/DistanceCalculator.java
@@ -0,0 +1,11 @@
+package com.easternedgerobotics.rov.math;
+
+import com.easternedgerobotics.rov.config.DistanceCalculatorConfig;
+
+public class DistanceCalculator {
+    private final DistanceCalculatorConfig config;
+
+    public DistanceCalculator(final DistanceCalculatorConfig config) {
+        this.config = config;
+    }
+}
