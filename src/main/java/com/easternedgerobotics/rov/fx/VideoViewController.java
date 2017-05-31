bb3e85061e8586835d4f0b3c366cd134969e9423
Cal Pratt <cpratt@mun.ca>
2017-05-31 18:11:49 -0230

    Properly create the video view using oncreate and ondestroy

--- src/main/java/com/easternedgerobotics/rov/fx/VideoViewController.java
+++ src/main/java/com/easternedgerobotics/rov/fx/VideoViewController.java
@@ -2,19 +2,35 @@ package com.easternedgerobotics.rov.fx;
 
 import com.easternedgerobotics.rov.video.VideoDecoder;
 
+import rx.subscriptions.CompositeSubscription;
+
 import javax.inject.Inject;
 
 public final class VideoViewController implements ViewController {
+    private CompositeSubscription subscription = new CompositeSubscription();
+
+    private final VideoDecoder decoder;
+
+    private final VideoView view;
+
     @Inject
     public VideoViewController(
         final VideoDecoder decoder,
         final VideoView view
     ) {
-        decoder.cameraAImages()
-            .repeat()
-            .subscribe(view.cameraA::setImage);
-        decoder.cameraBImages()
-            .repeat()
-            .subscribe(view.cameraB::setImage);
+        this.decoder = decoder;
+        this.view = view;
+    }
+
+    @Override
+    public void onCreate() {
+        subscription.addAll(
+            decoder.cameraAImages().observeOn(JAVA_FX_SCHEDULER).subscribe(view.cameraA::setImage),
+            decoder.cameraBImages().observeOn(JAVA_FX_SCHEDULER).subscribe(view.cameraB::setImage));
+    }
+
+    @Override
+    public void onDestroy() {
+        subscription.clear();
     }
 }
